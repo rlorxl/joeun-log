5ae95c724997eb32e10e1a6021094e38d1ef0fb5
rlorxl <gooodgirl08@gmail.com>
2023-09-11 02:04:35 +0900

    feat: seperate blog navigation

---
+++ src/app/blog/[...slug]/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const DetailPage = ({ params }: { params: { slug: string } }) => {
+  return <div>DetailPage</div>;
+};
+
+export default DetailPage;
--- src/app/blog/layout.tsx
+++ src/app/blog/layout.tsx
@@ -1,48 +1,19 @@
+'use client';
 import React from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
 import Logo from '../../../public/logo.svg';
-import * as Icon from '../../../public/icon/index';
-
-type TBlogNav = {
-  name: string;
-  link: string;
-};
-
-const navLinkto: TBlogNav[] = [
-  { name: '개발얘기', link: '/blog/develop' },
-  { name: '취준일기', link: '/blog/diary' },
-  { name: '그냥생각', link: '/blog/daily' },
-];
+import BlogNavigation from '@/components/blog/navigation';
 
 const BlogLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="flex justify-between items-start py-20 h-screen">
-      <nav className="flex-col flex justify-between items-start w-[120px] h-full">
-        <div>
-          <Link href="/blog">
-            <Image src={Logo} alt="logo" width={40} />
-          </Link>
-          <ul className="space-y-8 mt-16">
-            {navLinkto.map(({ name, link }) => (
-              <li key={name}>
-                <Link href={link}>{name}</Link>
-              </li>
-            ))}
-          </ul>
-        </div>
-        <div className="space-y-10 flex-col flex">
-          <button type="button">
-            <Image src={Icon.Search} alt="검색" />
-          </button>
-          <button type="button">
-            <Image src={Icon.Sun} alt="다크모드" />
-          </button>
-          <Link href="/">
-            <Image src={Icon.Door} alt="메인으로 이동" />
-          </Link>
-          <div className="mt-8 text-sm text-second-color">©rlorxl 2023</div>
-        </div>
+      <nav className="flex-col flex justify-between w-[120px] h-full">
+        <Link href="/blog" className="mb-8">
+          <Image src={Logo} alt="logo" width={40} />
+        </Link>
+        <BlogNavigation />
+        <div className="mt-8 text-sm text-second-color">©rlorxl 2023</div>
       </nav>
       {children}
     </div>
