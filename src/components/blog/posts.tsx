d1f0ddcd93fee027ff9be662765ce9a35a9bcf10
rlorxl <gooodgirl08@gmail.com>
2023-09-27 22:58:09 +0900

    post: Next.js 13

--- src/components/blog/posts.tsx
+++ src/components/blog/posts.tsx
@@ -72,7 +72,7 @@ const Posts = ({ posts }: { posts: TPost[] }) => {
             data-class="post"
             key={frontmatter.title}
             className={
-              'border-b last:border-b-0 pb-5 border-b-darkmode-text-color space-y-4 sm:px-8' +
+              'border-b last:border-b-0 pb-8 border-b-darkmode-text-color space-y-4 sm:px-8' +
               popupAni(idx)
             }>
             <h1 className="text-2xl font-semibold mb-4">
--- src/custom/mdx-styling.tsx
+++ src/custom/mdx-styling.tsx
@@ -82,8 +82,8 @@ const NavHeading = (
       data-id={text}
       className={
         `block cursor-pointer hover:text-second-color` +
-        (size === 'h2' ? ' ml-4' : '') +
-        (size === 'h3' ? ' ml-8' : '') +
+        (size === 'h2' ? ' ml-2' : '') +
+        (size === 'h3' ? ' ml-4' : '') +
         (text === content ? ' text-violet-400' : '')
       }
       onClick={scrollTo}
