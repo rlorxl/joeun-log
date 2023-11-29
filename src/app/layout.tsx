a985a745af65923ceb4f68cf59353b6a683b19b7
rlorxl <gooodgirl08@gmail.com>
2023-11-29 18:54:34 +0900

    title, description 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,8 +6,9 @@ import getCookie from '@/utils/common/get-cookie';
 import CheckDefaultScheme from '@/wrapper/default-scheme';
 
 export const metadata: Metadata = {
-  title: '조은씀',
-  description: '주니어 개발자 조은의 개발 블로그',
+  title: '주니어 프론트엔드 개발자 개인블로그: 조은씀',
+  description:
+    '주니어 개발자 조은의 개발 블로그입니다. 개발도중 새롭게 알게 된 내용에 대한 정리와 글을 공유합니다. 이 블로그는 NEXT.js의 app router를 활용하여 제작되었습니다.',
   keywords: ['개발블로그', '개발자 블로그', '프론트엔드'],
 
   icons: [
@@ -37,6 +38,7 @@ const RootLayout = ({ children }: { children: React.ReactNode }) => {
           name="google-site-verification"
           content="5Hoc5w7TAtymO_efS0zVrlyI3_ivotkExZNVKiPTs4Y"
         />
+        <link rel="canonical" href="https://rlorxl.me/blog"></link>
       </head>
       <CheckDefaultScheme cookie={theme}>
         <main className={`${sofia.className} w-full min-h-screen relative flex-col flex-center`}>
